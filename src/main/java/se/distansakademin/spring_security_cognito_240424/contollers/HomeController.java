7ce961b6fe14df0c29a95effa6fffe7a29f08947
linus-rudbeck <linus.rudbeck@distansakademin.se>
2024-05-08 08:46:05 +0200

    sign up m.m.

--- src/main/java/se/distansakademin/spring_security_cognito_240424/contollers/HomeController.java
+++ src/main/java/se/distansakademin/spring_security_cognito_240424/contollers/HomeController.java
@@ -2,6 +2,9 @@ package se.distansakademin.spring_security_cognito_240424.contollers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+// http://localhost:8080/
 
 @Controller
 public class HomeController {
@@ -14,5 +17,11 @@ public class HomeController {
     @GetMapping("/test")
     public String getTestPage(){
         return "test";
+    }
+
+    @PostMapping("/test")
+    public String postTest(){
+        System.out.println(">>>>>> POST: /test");
+        return "redirect:/test";
     }
 }
---
+++ src/main/java/se/distansakademin/spring_security_cognito_240424/contollers/SignUpController.java
@@ -0,0 +1,41 @@
+package se.distansakademin.spring_security_cognito_240424.contollers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import se.distansakademin.spring_security_cognito_240424.configurations.Cognito;
+import se.distansakademin.spring_security_cognito_240424.models.SignUpRequest;
+
+@Controller
+public class SignUpController {
+
+    @Value("${aws.cognito.poolId}")
+    private String poolId;
+
+    @Value("${aws.cognito.registration.clientId}")
+    private String clientId;
+
+
+    @GetMapping("/sign-up")
+    public String getSignUp(Model model){
+        model.addAttribute("signUp", new SignUpRequest());
+        return "sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String postSignUp(@ModelAttribute SignUpRequest request){
+
+        var username = request.getUsername();
+
+        var success = Cognito.signUp(clientId, username, request.getPassword(), request.getEmail());
+
+        if(success){
+            Cognito.confirmUser(poolId, username);
+        }
+
+        return "redirect:/";
+    }
+}
